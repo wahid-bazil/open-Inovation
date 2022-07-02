640494f2020fd7c3f04b2aa594854417489e7a57
CHAHDI MOHAMED-ext <mchahdi-ext@royalairmaroc.com>
2022-07-02 02:48:52 +0100

    Update src/component-slices/finalResult/index.tsx

--- src/component-slices/finalResult/index.tsx
+++ src/component-slices/finalResult/index.tsx
@@ -44,9 +44,10 @@ const FinalResult = () => {
                 <div className="top-side">
                     <ProjectClassement/>
                 </div>
-                <!--div className="bottom-side">
+                /*<div className="bottom-side">
                     <JuryClassement/>
-                </div>-->
+                </div>
+                */
             </Layout>
         </div>
     )
